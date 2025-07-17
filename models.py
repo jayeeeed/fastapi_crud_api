91c8023cc06a7921064833b2a32e887b8a459914
jayeeeed <jayed@myalice.ai>
2025-07-18 04:21:03 +0600

    id fix

--- models.py
+++ models.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.dialects.postgresql import UUID
 from database import Base
 import uuid
 
 
 class Item(Base):
     __tablename__ = "items"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
     name = Column(String, nullable=False, index=True)
     price = Column(Integer, nullable=False)
